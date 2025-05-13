2605e4a2b94fdb8b084752667380cfbbdb8c6430
Lab05_etec <Lab05_etec@etec>
2025-05-13 20:43:33 -0300

    novo projeto

--- componentes/header.php
+++ componentes/header.php
@@ -39,7 +39,7 @@
       </li>
       
       <li class="nav-item">
-        <a class="nav-link text-light" href="./dados/instalar.php" > 
+        <a class="nav-link text-light" href="./dados/instalar_BQ.php" > 
           Instalar o banco 
         </a>
       </li>
